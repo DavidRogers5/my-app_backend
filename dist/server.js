4383739a6cb2842c12d7adcae376606d427bc7ca
davidrogers5 <david.rogers5@hotmail.com>
2025-04-24 15:44:02 -0500

    4/24/25 3:43 p.m. CD

--- dist/server.js
+++ dist/server.js
@@ -1,61 +1,122 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _typeof = require("@babel/runtime/helpers/typeof");
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-var _hapi = _interopRequireWildcard(require("@hapi/hapi"));
+var _hapi = _interopRequireDefault(require("@hapi/hapi"));
 var _routes = _interopRequireDefault(require("./routes"));
-function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
-function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+// ================================
+// 🌐 LOCAL DEVELOPMENT VERSION
+// ✅ This block is ACTIVE (uncommented) for local dev
+// ❌ Comment this entire block before deploying
+// ================================
+
+// import Hapi, { server } from '@hapi/hapi';
+// import routes from './routes';
+// const DBConn = require("./DB.js");
+
+// var cors = require('cors');
+
+// const start = async () => 
+// {
+//     //Angular version
+//     /*
+//     const server = Hapi.server({
+//         port:8080,
+//         host: 'localhost' , 
+//         "routes": {
+//             "cors": {
+//                 "origin": ["http://localhost:4200"],
+//                 "headers": ["Accept", "Content-Type"],
+//                 "additionalHeaders": ["X-Requested-With"]
+//             }
+//         }
+//     */
+
+//     //React version
+//     const server = Hapi.server({
+//         port:8080,
+//         host: 'localhost' , 
+//         "routes": {
+//             "cors": {
+//                 "origin": ["http://localhost:3000"],
+//                 "headers": ["Accept", "Content-Type"],
+//                 "additionalHeaders": ["X-Requested-With"]
+//             }
+//         }
+//     });
+
+//     //Angular app IP is http://localhost:4200 , React is 3000
+
+//     routes.forEach(routes => server.route(routes));
+
+//     server.route({
+//         method: 'GET',
+//         path: '/hello',
+//         handler: (req, h) => {  
+//             return 'Hello';        
+//         }
+//     });
+
+//     await server.start();
+//     console.log('Server is listening on ${server.info.uri}');
+// }
+
+// process.on('unhandledRejection' , err => {
+//     console.log(err);
+//     process.exit(1);
+// });
+
+// start();
+
+// ================================
+// ☁️ AZURE DEPLOYMENT VERSION
+// ❌ This block is currently commented out
+// ✅ Uncomment this block when deploying to Azure
+// ================================
+
 var DBConn = require("./DB.js");
-var cors = require('cors');
 var start = /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])(/*#__PURE__*/_regenerator["default"].mark(function _callee() {
     var server;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) switch (_context.prev = _context.next) {
         case 0:
-          //Angular version
-          /*
-          const server = Hapi.server({
-              port:8080,
-              host: 'localhost' , 
-              "routes": {
-                  "cors": {
-                      "origin": ["http://localhost:4200"],
-                      "headers": ["Accept", "Content-Type"],
-                      "additionalHeaders": ["X-Requested-With"]
-                  }
-              }
-          */
-          //React version
           server = _hapi["default"].server({
-            port: 8080,
-            host: 'localhost',
-            "routes": {
-              "cors": {
-                "origin": ["http://localhost:3000"],
-                "headers": ["Accept", "Content-Type"],
-                "additionalHeaders": ["X-Requested-With"]
+            port: process.env.PORT || 8080,
+            host: '0.0.0.0',
+            // Required for Azure
+            routes: {
+              cors: {
+                origin: ['*'],
+                headers: ['Accept', 'Content-Type'],
+                additionalHeaders: ['X-Requested-With']
               }
             }
-          }); //Angular app IP is http://localhost:4200 , React is 3000
-          _routes["default"].forEach(function (routes) {
-            return server.route(routes);
+          });
+          _routes["default"].forEach(function (route) {
+            return server.route(route);
+          });
+          server.route({
+            method: 'GET',
+            path: '/',
+            handler: function handler() {
+              console.log("🔁 Root route hit");
+              return 'API is live!';
+            }
           });
           server.route({
             method: 'GET',
             path: '/hello',
-            handler: function handler(req, h) {
-              return 'Hello';
+            handler: function handler() {
+              return 'Hello from Azure!';
             }
           });
-          _context.next = 5;
+          _context.next = 6;
           return server.start();
-        case 5:
-          console.log('Server is listening on ${server.info.uri}');
         case 6:
+          console.log("\u2705 Server is running at ".concat(server.info.uri));
+        case 7:
         case "end":
           return _context.stop();
       }
--- src/server.js
+++ src/server.js
@@ -1,60 +1,118 @@
-import Hapi, { server } from '@hapi/hapi';
+// ================================
+// 🌐 LOCAL DEVELOPMENT VERSION
+// ✅ This block is ACTIVE (uncommented) for local dev
+// ❌ Comment this entire block before deploying
+// ================================
+
+
+// import Hapi, { server } from '@hapi/hapi';
+// import routes from './routes';
+// const DBConn = require("./DB.js");
+
+// var cors = require('cors');
+
+// const start = async () => 
+// {
+//     //Angular version
+//     /*
+//     const server = Hapi.server({
+//         port:8080,
+//         host: 'localhost' , 
+//         "routes": {
+//             "cors": {
+//                 "origin": ["http://localhost:4200"],
+//                 "headers": ["Accept", "Content-Type"],
+//                 "additionalHeaders": ["X-Requested-With"]
+//             }
+//         }
+//     */
+    
+//     //React version
+//     const server = Hapi.server({
+//         port:8080,
+//         host: 'localhost' , 
+//         "routes": {
+//             "cors": {
+//                 "origin": ["http://localhost:3000"],
+//                 "headers": ["Accept", "Content-Type"],
+//                 "additionalHeaders": ["X-Requested-With"]
+//             }
+//         }
+//     });
+
+//     //Angular app IP is http://localhost:4200 , React is 3000
+
+//     routes.forEach(routes => server.route(routes));
+
+//     server.route({
+//         method: 'GET',
+//         path: '/hello',
+//         handler: (req, h) => {  
+//             return 'Hello';        
+//         }
+//     });
+
+//     await server.start();
+//     console.log('Server is listening on ${server.info.uri}');
+// }
+
+// process.on('unhandledRejection' , err => {
+//     console.log(err);
+//     process.exit(1);
+// });
+
+// start();
+
+
+
+// ================================
+// ☁️ AZURE DEPLOYMENT VERSION
+// ❌ This block is currently commented out
+// ✅ Uncomment this block when deploying to Azure
+// ================================
+
+
+import Hapi from '@hapi/hapi';
 import routes from './routes';
 const DBConn = require("./DB.js");
 
-var cors = require('cors');
-
-const start = async () => 
-{
-
-    //Angular version
-    /*
+const start = async () => {
     const server = Hapi.server({
-        port:8080,
-        host: 'localhost' , 
-        "routes": {
-            "cors": {
-                "origin": ["http://localhost:4200"],
-                "headers": ["Accept", "Content-Type"],
-                "additionalHeaders": ["X-Requested-With"]
+        port: process.env.PORT || 8080,
+        host: '0.0.0.0', // Required for Azure
+        routes: {
+            cors: {
+                origin: ['*'],
+                headers: ['Accept', 'Content-Type'],
+                additionalHeaders: ['X-Requested-With']
             }
         }
-    */
+    });
 
-    //React version
-    const server = Hapi.server({
-        port:8080,
-        host: 'localhost' , 
-        "routes": {
-            "cors": {
-                "origin": ["http://localhost:3000"],
-                "headers": ["Accept", "Content-Type"],
-                "additionalHeaders": ["X-Requested-With"]
-            }
+    routes.forEach(route => server.route(route));
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: () => {
+            console.log("🔁 Root route hit");
+            return 'API is live!';
         }
+    });
 
-});
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: () => {
+            return 'Hello from Azure!';
+        }
+    });
 
-//Angular app IP is http://localhost:4200 , React is 3000
+    await server.start();
+    console.log(`✅ Server is running at ${server.info.uri}`);
+};
 
-routes.forEach(routes => server.route(routes));
-
-server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: (req, h) => {  
-        
-        return 'Hello';        
-
-    }
-});
-
-
-await server.start();
-console.log('Server is listening on ${server.info.uri}');
-}
-
-process.on('unhandledRejection' , err => {
+process.on('unhandledRejection', err => {
     console.log(err);
     process.exit(1);
 });
